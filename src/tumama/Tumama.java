458a15591f30b51ec550856432d0e21a3cfb26c7
ThebeatYL <thebeatyl@gmail.com>
2024-05-13 01:14:35 -0600

    Update Tumama.java
    a

--- src/tumama/Tumama.java
+++ src/tumama/Tumama.java
@@ -14,7 +14,9 @@ public class Tumama {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        SystemOutPrintLn("holaquehace");
+        System.out.println("lkjashfkjlashdfkjlahslkjdfjlkasdjfalskjdfksd");
+        
+       
     }
     
 }
